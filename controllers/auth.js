e48139ceebf9e8ceb358536ea815d0915eac6076
miha11-hash <d_mihaela@msn.com>
2023-09-19 08:29:39 +0300

    mongoose middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,19 +6,17 @@ const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
     //res.send("register user");
-    const {name, email, password}=req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword= await bcrypt.hash(password, salt)
-
-    const tempUser = {name:name, email:email, password: hashedPassword};
-
-
+    // const {name, email, password}=req.body;
+    // const salt = await bcrypt.genSalt(10);
+    // const hashedPassword= await bcrypt.hash(password, salt);
+    //const tempUser = {name:name, email:email, password: hashedPassword};
     //console.log(req.body);
     //res.json(req.body);
-    if(!name || !email || !password) {
-        throw new BadRequest("Please provide name, email and password!");
-    }
-    const user = await User.create({...tempUser});
+    // if(!name || !email || !password) {
+    //     throw new BadRequest("Please provide name, email and password!");}
+    // const user = await User.create({tempUser});
+    const user = await User.create({...req.body});
+
     res.status(StatusCodes.CREATED).json({user});
     }
 
