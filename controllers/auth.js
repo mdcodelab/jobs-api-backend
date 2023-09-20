c868866e6ca016e013e3a0173c0ce1ead0e7c1d2
miha11-hash <d_mihaela@msn.com>
2023-09-20 12:48:40 +0300

    JWT_SECRET and JWT_LIFETIME

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const BadRequest=require("../errors/bad-request");
 const {StatusCodes}=require("http-status-codes");
 //const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const register = async (req, res) => {
     //res.send("register user");
@@ -17,7 +18,8 @@ const register = async (req, res) => {
     //     throw new BadRequest("Please provide name, email and password!");}
     // const user = await User.create({tempUser});
     const user = await User.create({...req.body});
-    const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", { expiresIn: "30d" });
+    const token = jwt.sign({ userId: user._id, name: user.name },
+         process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME });
 
     res.status(StatusCodes.CREATED).json({user, token});
     }
