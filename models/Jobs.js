0326c9dfdb0dd36fb9a2daf90c58ce42b74a40e4
miha11-hash <d_mihaela@msn.com>
2023-09-21 11:53:29 +0300

    JobsSchema

--- models/Jobs.js
+++ models/Jobs.js
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose");
 
-const JobsSchema = new mongoose.Schema ({
-company: {
+const JobsSchema = new mongoose.Schema({
+  company: {
     type: String,
-    required: true,
-    maxLength: 50
-},
+    required: [true, "Please provide company's name"],
+    maxLength: 50,
+  },
+  position: {
+    type: String,
+    required: [true, "Please provide position"],
+    maxLength: 100,
 
-})
+  },
+  status: {
+    type: String,
+    enum: ["interview", "declined", "pending"],
+    default: "pending"
+  },
+  createdBy: {  //meaning that every time when we create a job, we'll assign it to one of the users
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please provide a user"]
+  }
+}, {timestamps: true});
 
-module.exports = JobsSchema;
+module.exports = mongoose.model("Jobs", JobsSchema);
+
