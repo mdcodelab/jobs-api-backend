e8db1922c672f848212bc847deb8a447fe668f92
miha11-hash <d_mihaela@msn.com>
2023-09-23 09:35:13 +0300

    updateJob (controllers)

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -26,7 +26,6 @@ const getJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ jobs });
 };
 
-
 // or:
 // const getJob = async (req, res) => {
 //   const job = await Job.find({ createdBy: req.user.userId });
@@ -46,9 +45,53 @@ const createJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({job})
 };
 
+
+// const updateJob = async (req, res) => {
+//   const jobId = req.params.id;
+//   const userId = req.user.userId;
+//   const { company, position } = req.body;
+
+//   const jobToUpdate = await Job.findOne({ _id: jobId, createdBy: userId });
+
+//   if (!jobToUpdate) {
+//     throw new NotFound(
+//       "The job was not found for the specified user or does not exist"
+//     );
+//   }
+
+//   jobToUpdate.company = company;
+//   jobToUpdate.position = position;
+
+//   const jobUpdated = await jobToUpdate.save();
+
+//   res.status(StatusCodes.ACCEPTED).json({jobUpdated})
+// };
+
+//or
 const updateJob = async (req, res) => {
-  res.send("job updated");
+  const jobId = req.params.id;
+  const userId = req.user.userId;
+  const { company, position } = req.body;
+  //verify for the existing job and whether it has been created by the user with the specific id
+  const existingJob = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!existingJob) {
+    throw new NotFound("Job not found or not authorized to update");
+  }
+
+  const updatedValues = {
+    company: company,
+    position: position
+  };
+
+  const jobUpdated = await Job.findByIdAndUpdate(jobId, updatedValues, { new: true });
+  res.status(StatusCodes.ACCEPTED).json({ jobUpdated });
 };
+
+
+
+
+
+
 
 const deleteJob = async (req, res) => {
   res.send("job deleted");
